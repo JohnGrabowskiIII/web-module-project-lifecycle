9aaa46700454528b7ea189261b1d97f1f79d9e66
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-06-03 12:47:31 -0400

    turned app.js into a class component skeleton with comments for future work. Installed and imported axios

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,11 +1,26 @@
 
+import axios from 'axios'
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    // FILTER FUNCTION FOR AXIOS DATA
+    // FUNCTION TO APPEND FOLLOWERS TO STATE
+    // FUNCTION TO MAP STATE AND RETURN CARD JSX
+  }
+
+  componentDidMount() {
+    // AXIOS CALL GITHUB INFO
+      // THEN SET STATE
+      // THEN AXIOS CALL FOLLOWERS
+        // FUNCTION TO APPEND TO STATE
+  }
+
+  render() {
+    // MAP STATE FUNCTION
+  }
+
 }
 
 export default App;
